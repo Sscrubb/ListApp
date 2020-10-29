d43c331bbc5e9cdf2ed3819f1330e921ee0c49f3
Sscrubb <yulia.kiryushina@github.com>
2020-10-29 13:28:11 +0300

    login binding

--- ListApp/ViewModels/LoginViewModel.cs
+++ ListApp/ViewModels/LoginViewModel.cs
@@ -8,5 +8,18 @@ namespace ListApp.ViewModels
         public LoginViewModel(ViewModelManager paramter) : base(paramter)
         {
         }
+
+        private string _login;
+        public string Login 
+        { 
+            get => _login;
+            set {
+                if (_login != value)
+                {
+                    _login = value;
+                    this.GeneratePropertyChangedEvent(nameof(Login));
+                }
+            }
+        }
     }
 }
